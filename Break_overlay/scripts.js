5477d5ade4b43d65458e525a74f39ba6ccba501c
KobeLiesenborgs <kobe.liesenborgs1998@gmail.com>
2020-04-23 09:27:12 +0200

    Timer

---
+++ Break_overlay/scripts.js
@@ -0,0 +1,80 @@
+window.onload = () => {
+    console.log("Timeout loaded")
+    const socket = io.connect("localhost:3000");
+    let startDate = new Date();
+    let goal = startDate.getTime();
+    let text;
+    let textField = document.getElementById("t1");
+
+    console.log(textField);
+
+    document
+    countLoop()
+    
+
+    function countLoop(){
+        var d = new Date();
+        var now = d.getTime();
+
+        let currentWaitTime = (goal-now)
+        S = Math.abs((currentWaitTime/1000|0)%60)
+        M = Math.abs((currentWaitTime/60000|0)%60)
+
+        M = (""+M).padStart(2,"0")
+        S = (""+S).padStart(2,"0")
+        
+        if(currentWaitTime>0){
+
+            text = `Quick break, will restart in ${M} minutes ${S} seconds.`;
+
+        }
+        else{
+
+            text = `Should have started ${M} minutes and ${S} seconds ago.`;
+
+        }
+
+        textField.style.textAlign = "center";
+        textField.textContent = text;
+
+
+
+        requestAnimationFrame(countLoop);
+    }
+
+    socket.on("message",(input)=>{
+
+        const tags = input.tags;
+        let [text, minutes] = input.message.split(" ");
+
+
+        if(text.toLowerCase() == "timeout" && ["broadcaster"].some((type)=>(tags["badges"]||{})[type])){
+            if(!minutes){
+                minutes = 30
+            }
+            goal = new Date().getTime() + 60000*minutes
+        }
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
